9fcc1269d9801ef73fdf68f6d4c22037ac8ae5d3
Justin Williams <williams.jman@gmail.com>
2022-12-15 21:51:34 -0500

    Created SQL to show all the last name and first names of the students and emails

--- queries.sql
+++ queries.sql
@@ -3,3 +3,6 @@ INSERT INTO students (first_name, last_name) values ('Dan', 'Zanes');
 
 --Requirement 2
 UPDATE students SET email = 'jblair21@college.edu' WHERE id = 1;
+
+--Requirement 3
+select concat(last_name,' ', first_name) as name , email from students ORDER BY last_name
