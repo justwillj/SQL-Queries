719fb96bfcbaf5e096d167f29d8cdfc2b024ee79
Justin Williams <williams.jman@gmail.com>
2022-12-16 13:13:05 -0500

    Created the SQL to see all of his grades and class name

--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ UPDATE students SET email = 'jblair21@college.edu' WHERE id = 1;
 
 --Requirement 3
 select concat(last_name,' ', first_name) as name , email from students ORDER BY last_name
+
+--Requirement 4
+SELECT classes.name, grades.grade
+FROM classes
+INNER JOIN grades
+ON classes.id = grades.class_id
+WHERE student_id = 3;
