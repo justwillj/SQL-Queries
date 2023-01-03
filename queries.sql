a0f429977884b5113c26c892055df93ff0264e81
Tiana Langer <langert@midlandstech.edu>
2023-01-03 19:23:03 +0000

    code-review

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
 --Requirement 1
+--CODE REVIEW: if your data is following a trend, it may be best to enter in as much information as you can, given you
+-- have permission to do so. 
 INSERT INTO students (first_name, last_name) values ('Dan', 'Zanes');
 
 --Requirement 2
 UPDATE students SET email = 'jblair21@college.edu' WHERE id = 1;
 
 --Requirement 3
+--CODE REVIEW: format was to be <lastname, firstname> not <lastname firstname> BE MINDFUL!
 select concat(last_name,' ', first_name) as name , email from students ORDER BY last_name
 
 --Requirement 4
